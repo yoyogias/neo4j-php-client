ddb47b66405674a8c0d2099419f2480b1dd305a3
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-02-27 08:26:40 +0100

    Added return type annotation

--- src/Transaction/Transaction.php
+++ src/Transaction/Transaction.php
@@ -12,6 +12,7 @@
 namespace GraphAware\Neo4j\Client\Transaction;
 
 use GraphAware\Common\Cypher\Statement;
+use GraphAware\Common\Result\Result;
 use GraphAware\Common\Transaction\TransactionInterface;
 use GraphAware\Neo4j\Client\Event\PostRunEvent;
 use GraphAware\Neo4j\Client\Event\PreRunEvent;
@@ -92,7 +93,7 @@ class Transaction
     /**
      * @param StackInterface $stack
      *
-     * @return mixed
+     * @return ResultCollection|Result[]
      */
     public function runStack(StackInterface $stack)
     {
